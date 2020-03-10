03d253593ed50be7df5687902f13c0b588f15994
dephizee <hafiz227@gmail.com>
2020-03-10 14:59:06 +0100

    :)

--- app/controllers/About.php
+++
@@ -1,7 +0,0 @@
-<?php 
-class About extends Controller{	
-	function index(){
-		$this->view('About', [1,'aa']);
-	}	
-}
- ?>
--- app/controllers/Naomi.php
+++
@@ -1,7 +0,0 @@
-<?php 
-class Naomi extends Controller{	
-	function index(){
-		$this->view('Naomi', [1,'aa']);
-	}	
-}
- ?>
